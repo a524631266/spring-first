f7ee1d94e0912ea264e37da5be69cac10817b818
zhangll <zhangll@homewell.com.cn>
2019-01-20 16:05:13 +0800

    添加学习反射的内容

--- playground/src/test/java/com/home/java/fundament/playground/DemoApplicationTests2.java
+++ playground/src/test/java/com/home/java/fundament/playground/DemoApplicationTests2.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Field;
 
 import com.home.java.fundament.playground.annotationlearning.exercise.HomeField;
 import com.home.java.fundament.playground.annotationlearning.exercise.HomeTable;
-import com.home.java.fundament.playground.annotationlearning.exercise.Student;
+// import com.home.java.fundament.playground.annotationlearning.exercise.Student;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,9 +26,13 @@ public class DemoApplicationTests2 {
 
 	/**
 	 * 使用反射读取注解的信息，模拟流程
+	 * 
+	 * @throws SecurityException
+	 * @throws NoSuchFieldException
 	 */
+	@SuppressWarnings("all")
 	@Test
-	public void testParseStudentAnnotation() {
+	public void testParseStudentAnnotation() throws NoSuchFieldException, SecurityException {
 		// Student student = new Student();
 		// System.out.println(student); // { age='0', studentName='null', id='0'}
 		try {
@@ -39,7 +43,7 @@ public class DemoApplicationTests2 {
 			}
 
 			HomeTable st = (HomeTable) clazz.getAnnotation(HomeTable.class);
-			// 获取其值
+			// get the value
 			System.out.println(st.value());
 			System.out.println("+++++++++++++++++++++++++++++++++");
 			// // 获取类属性的注解
@@ -49,11 +53,17 @@ public class DemoApplicationTests2 {
 			// System.out.println(homeField.datatype());
 			// System.out.println(homeField.columnName());
 
-			Field[] homefields = clazz.getFields();
-			for (Field homefield : homefields) {
+			Field[] studentfields = clazz.getDeclaredFields();
+			for (Field homefield : studentfields) {
+				System.out.println("-----------------------------\r\n");
 				HomeField hf = homefield.getAnnotation(HomeField.class);
-				System.out.println(hf.columnName() + hf.datatype() + hf.length());
+				System.out.println(hf.columnName() + hf.datatype() + hf.length() + "\r\n");
 			}
+			System.out.println("++++++++++++++获取当个注解+++++++++++++++++++");
+			// 获取单个属性注解
+			Field stf = clazz.getDeclaredField("studentName");
+			HomeField stff = stf.getAnnotation(HomeField.class);
+			System.out.println(stff.columnName() + stff.datatype() + stff.length() + "\r\n");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 
@@ -70,6 +80,11 @@ public class DemoApplicationTests2 {
 		Zi zi = (Zi) f;
 		System.out.println(zi.num); // 100
 		zi.show(); // 10
+	}
+
+	public static void main(String[] args) {
+		// testduotai();
+		System.out.println("1111");
 	}
 }
 
