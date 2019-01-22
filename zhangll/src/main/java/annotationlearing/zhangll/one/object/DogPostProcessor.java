b0ca826db326f92c2fa2fa17a24fff08d8874384
zhangll <524631266@qq.com>
2019-01-22 20:47:14 +0800

    bean的生命周期4 后置处理bean

---
+++ zhangll/src/main/java/annotationlearing/zhangll/one/object/DogPostProcessor.java
@@ -0,0 +1,24 @@
+package annotationlearing.zhangll.one.object;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * aspectj 方式织入bean
+ */
+@Component
+public class DogPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("post销毁之前" + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        System.out.println("post 初始化之前 构造之后" + beanName);
+        return bean;
+    }
+}
